1a8afffaaa409f0cf7c11f47661127497958cdf1
AndrianoTurner <danya.shibaev@gmail.com>
2024-03-28 13:32:51 +0300

    Model fm

--- modulation.py
+++ modulation.py
@@ -3,74 +3,98 @@ import matplotlib.pyplot as plt
 import wave
 import tkinter as tk
 from tkinter import messagebox
+from scipy.interpolate import interp1d
 
-def generate_signal(bits):
-    bit_rate = 500  # бит в секунду
-    carrier_frequency = 2000  # Гц (частота несущей)
-    sampling_rate = 8000  # отсчетов в секунду
+class SignalProcessor():
+    def __init__(self,bits,bit_rate,carrier_frequency,sampling_rate) -> None:
+        self.bits = bits
+        self.carrier_frequency = carrier_frequency
+        self.bit_rate = bit_rate
+        self.sampling_rate = sampling_rate
+        self.time_per_bit = 1 / bit_rate
+        self.time = np.linspace(0, len(self.bits) * self.time_per_bit, int(len(self.bits) * self.time_per_bit * sampling_rate), endpoint=False)
+        self.amplitude = 100
+        
 
-    # Вычисление времени на один бит
-    time_per_bit = 1 / bit_rate
-    # Создание временной оси
-    time = np.linspace(0, len(bits) * time_per_bit, int(len(bits) * time_per_bit * sampling_rate), endpoint=False)
-
-    # Амплитуда сигнала
-    amplitude = 100
-
-    # Пустой массив для сигнала
-    signal = np.zeros(len(time))
-
-    for i, bit in enumerate(bits):
-        if bit == 1:
-            # Добавление синусоиды для бита "1"
-            signal[i * int(time_per_bit * sampling_rate): (i + 1) * int(time_per_bit * sampling_rate)] = \
-                amplitude * np.sin(2 * np.pi * carrier_frequency * time[i * int(time_per_bit * sampling_rate): (i + 1) * int(time_per_bit * sampling_rate)])
-        else:
-            # Установка значения сигнала равным половине амплитуды для бита "0"
-            signal[i * int(time_per_bit * sampling_rate): (i + 1) * int(time_per_bit * sampling_rate)] = \
-                0.5 * amplitude * np.sin(2 * np.pi * carrier_frequency * time[i * int(time_per_bit * sampling_rate): (i + 1) * int(time_per_bit * sampling_rate)])
-
-    return signal
-
+    def generate_am_signal(self):
+        # Пустой массив для сигнала
+        signal = np.zeros(len(self.time))
+       
+        for i, bit in enumerate(self.bits):
+            time_start = i * int(self.time_per_bit * self.sampling_rate)
+            time_end = (i + 1) * int(self.time_per_bit * self.sampling_rate)
+            if bit == 1:
+                # Добавление синусоиды для бита "1"
+                signal[time_start:time_end] = \
+                    self.amplitude * np.sin(2 * np.pi * self.carrier_frequency * self.time[time_start:time_end])
+            else:
+                # Установка значения сигнала равным половине амплитуды для бита "0"
+                signal[time_start:time_end] = \
+                    0.5 * self.amplitude * np.sin(2 * np.pi * self.carrier_frequency * self.time[time_start:time_end])
+        return signal
+    
+    def generate_fm_signal(self,freq0,freq1):
+        signal = np.zeros(len(self.time))
+        for i, bit in enumerate(self.bits):
+            time_start = i * int(self.time_per_bit * self.sampling_rate)
+            time_end = (i + 1) * int(self.time_per_bit * self.sampling_rate)
+            if bit == 1:
+                # Добавление синусоиды для бита "1"
+                signal[time_start:time_end] = \
+                    self.amplitude * np.cos(2 * np.pi * freq0 * self.time[time_start:time_end])
+            else:
+                # Установка значения сигнала равным половине амплитуды для бита "0"
+                signal[time_start:time_end] = \
+                    self.amplitude * np.cos(2 * np.pi * freq1 * self.time[time_start:time_end])
+        return signal
 
 # Функция для сохранения сигнала в файл WAV
-def save_wav(filename, signal, sampling_rate):
-    normalized_signal = signal / np.max(np.abs(signal))  # Нормализация сигнала
-    signal = (normalized_signal * 32767).astype(np.int16)  # Приведение к 16-битному целому типу
-    with wave.open(filename, 'wb') as wave_file:
-        wave_file.setnchannels(1)  # Один канал (моно)
-        wave_file.setsampwidth(2)  # 2 байта на сэмпл (16 бит)
-        wave_file.setframerate(sampling_rate)  # Частота дискретизации
-        wave_file.writeframes(signal.tobytes())  # Запись сигнала в файл
+    def save_wav(self,filename, signal):
+        normalized_signal = signal / np.max(np.abs(signal))  # Нормализация сигнала
+        signal = (normalized_signal * 32767).astype(np.int16)  # Приведение к 16-битному целому типу
+        with wave.open(filename, 'wb') as wave_file:
+            wave_file.setnchannels(1)  # Один канал (моно)
+            wave_file.setsampwidth(2)  # 2 байта на сэмпл (16 бит)
+            wave_file.setframerate(self.sampling_rate)  # Частота дискретизации
+            wave_file.writeframes(signal.tobytes())  # Запись сигнала в файл
 
 
-# Функция для построения визуализации сигнала
-def plot_waveform(signal, time, interpolated_signal):
-    plt.figure(figsize=(10, 6))
-    plt.subplot(2, 1, 1)
-    plt.plot(time, signal, 'b-', label='Дискретные отсчеты')
-    plt.title('Сигнал')
-    plt.xlabel('Время (мс)')
-    plt.ylabel('Амплитуда')
-    plt.grid(True)
-    plt.legend()
+    # Функция для построения визуализации сигнала
+    def plot_waveform(self,signal,time,interpolated_signal,time_interp):
+        # fig, ax = plt.subplots(2, 1, num=0, clear=True, sharex=True)
+        # ax[0].set_title('Signal')
+        # ax[0].plot(t, sig)
+        # ax[1].set_title('Modulated Signal')
+        # ax[1].plot(t, sig_mod)
+        # ax[1].set_xlabel('Time')
 
-    plt.subplot(2, 1, 2)
-    time_interp = np.linspace(0, time[-1], len(interpolated_signal))  # Временная ось для интерполированного сигнала
-    plt.step(time_interp, interpolated_signal, 'r-', label='Интерполированный сигнал')
-    plt.title('Интерполированный сигнал')
-    plt.xlabel('Время (мс)')
-    plt.ylabel('Амплитуда')
-    plt.grid(True)
-    plt.legend()
+        plt.figure(figsize=(10, 6))
+        plt.subplot(2, 1, 1)
+        plt.plot(time, signal, 'b.', label='Дискретные отсчеты')
+        plt.title('Сигнал')
+        plt.xlabel('Время (мс)')
+        plt.ylabel('Амплитуда')
+        plt.grid(True)
+        plt.legend()
 
-    plt.tight_layout()
-    plt.show()
+        plt.subplot(2, 1, 2)
+
+        plt.plot(time_interp, interpolated_signal, 'r-', label='Интерполированный сигнал')
+        plt.title('Интерполированный сигнал')
+        plt.xlabel('Время (мс)')
+        plt.ylabel('Амплитуда')
+        plt.grid(True)
+        plt.legend()
+
+        plt.tight_layout()
+        plt.show()
 
 
 # Функция, которая вызывается при нажатии кнопки "Сгенерировать и сохранить"
 def generate_and_save():
+    
     bits_str = entry.get()
+   
     try:
         # Проверяем длину введенной битовой последовательности
         if len(bits_str) != 12:
@@ -79,17 +103,25 @@ def generate_and_save():
         # Преобразуем строку с битами в список целых чисел
         bits = [int(bit) for bit in bits_str]
 
-        sampling_rate = 8000
-        signal = generate_signal(bits)
-        time = np.arange(0, len(signal)) * 1000 / sampling_rate
-
+        processor = SignalProcessor(bits,500,2000,120_000)
+        signal = processor.generate_am_signal()
+        #
+# Значения несущей в Гц в звуковой полосе частот 
+        signal_fm = processor.generate_fm_signal(800,1600)
+        time = np.arange(0, len(signal)) * 1000 / processor.sampling_rate
+        time_interp = np.linspace(0, time[-1], 10 * len(time))  # Временная ось для интерполированного сигнала
         # Интерполяция сигнала
-        interpolated_signal = np.interp(time, time, signal)
+        interpolator_am = interp1d(time,signal,'cubic',bounds_error=False)
+        interpolated_am_signal = interpolator_am(time_interp)
 
-        save_wav('output.wav', signal, sampling_rate)
+        interpolator_fm = interp1d(time,signal_fm,'cubic',bounds_error=False)
+        interpolated_fm_signal = interpolator_fm(time_interp)
 
-        messagebox.showinfo("Успех", "Файл успешно сохранен как output.wav")
-        plot_waveform(signal, time, interpolated_signal)
+        #save_wav('output.wav', signal, sampling_rate)
+
+        #messagebox.showinfo("Успех", "Файл успешно сохранен как output.wav")
+        processor.plot_waveform(signal, time, interpolated_am_signal,time_interp)
+        processor.plot_waveform(signal_fm, time, interpolated_fm_signal,time_interp)
 
     except ValueError as e:
         messagebox.showerror("Ошибка", str(e))
@@ -103,6 +135,7 @@ label = tk.Label(app, text="Введите битовую последовате
 label.pack()
 
 entry = tk.Entry(app)
+entry.insert(0,"000110001100")
 entry.pack()
 
 button = tk.Button(app, text="Сгенерировать и сохранить", command=generate_and_save)
